8de06a111db966d2668b67f35619f0fbb6f4ceba
muhammet-enes-aksoy <muhammed_enes43@hotmail.com>
2024-01-17 23:50:46 +0300

    Create BaseRequest.cs

---
+++ ExpensePaymentSystem/ExpensePaymentSystem.Base/Schema/BaseRequest.cs
@@ -0,0 +1,8 @@
+namespace ExpensePaymentSystem.Base.Schema;
+// Represents an abstract base class for request objects.
+public abstract class BaseRequest
+{
+    // The class is currently empty, allowing derived classes to define specific properties or methods.
+    // It serves as a common base class for request objects in the application.
+}
+

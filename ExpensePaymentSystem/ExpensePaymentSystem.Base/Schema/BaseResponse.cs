2ce4dfda0fafe7699773dd2c7777386e20d9e62c
muhammet-enes-aksoy <muhammed_enes43@hotmail.com>
2024-01-17 23:50:48 +0300

    Create BaseResponse.cs

---
+++ ExpensePaymentSystem/ExpensePaymentSystem.Base/Schema/BaseResponse.cs
@@ -0,0 +1,8 @@
+namespace ExpensePaymentSystem.Base.Schema;
+// Represents an abstract base class for response objects.
+public abstract class BaseResponse
+{
+    // The class is currently empty, allowing derived classes to define specific properties or methods.
+    // It serves as a common base class for response objects in the application.
+}
+

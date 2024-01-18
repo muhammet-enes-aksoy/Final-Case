c00df3554c8e63ae54e6e63c98ddf6a3960cf398
muhammet-enes-aksoy <muhammed_enes43@hotmail.com>
2024-01-19 00:28:06 +0300

    Create User Schema

---
+++ ExpensePaymentSystem/ExpensePaymentSystem.Schema/User.cs
@@ -0,0 +1,26 @@
+using System.Text.Json.Serialization;
+using ExpensePaymentSystem.Base.Schema;
+using ExpensePaymentSystem.Data.Entity;
+
+namespace ExpensePaymentSystem.Schema;
+public class UserRequest : BaseRequest
+{
+    [JsonIgnore]
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Role { get; set; }
+}
+public class UserResponse : BaseResponse
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Role { get; set; }
+    public DateTime LastActivityDate { get; set; }
+    public int Status { get; set; }
+
+    public virtual List<Contact> Contacts { get; set; }
+    public virtual List<Account> Accounts { get; set; }
+    public virtual List<Address> Addresses { get; set; }
+    public virtual List<ExpenseClaim> ExpenseClaims { get; set; }
+}

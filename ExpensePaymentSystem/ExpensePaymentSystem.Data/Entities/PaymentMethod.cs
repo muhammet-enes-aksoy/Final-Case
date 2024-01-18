f8b1e76b76a6d85e59d2a26b3b9bfaeea505f7ec
muhammet-enes-aksoy <muhammed_enes43@hotmail.com>
2024-01-19 00:00:54 +0300

    Create PaymentMethod.cs

---
+++ ExpensePaymentSystem/ExpensePaymentSystem.Data/Entities/PaymentMethod.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using ExpensePaymentSystem.Base.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace ExpensePaymentSystem.Data.Entity;
+
+[Table("PaymentMethod", Schema = "dbo")]
+public class PaymentMethod : BaseEntityWithId
+{
+    public string PaymentMethodType { get; set; }
+    
+}
+
+public class PaymentMethodClaimConfiguration : IEntityTypeConfiguration<PaymentMethod>
+{
+    public void Configure(EntityTypeBuilder<PaymentMethod> builder)
+    {
+        builder.Property(x => x.InsertDate).IsRequired(true);
+        builder.Property(x => x.InsertUserId).IsRequired(true);
+        builder.Property(x => x.UpdateDate).IsRequired(false);
+        builder.Property(x => x.UpdateUserId).IsRequired(false);
+        builder.Property(x => x.IsActive).IsRequired(true).HasDefaultValue(true);
+    
+        builder.Property(x => x.PaymentMethodType).IsRequired(true).HasMaxLength(50);
+
+        //builder.HasIndex(x => x.UserName).IsUnique(true);
+
+    }
+}

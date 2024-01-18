11a9d092d871366fa3eea6d2874ce1f22ca43f16
muhammet-enes-aksoy <muhammed_enes43@hotmail.com>
2024-01-19 00:26:48 +0300

    Create Category.cs

---
+++ ExpensePaymentSystem/ExpensePaymentSystem.Schema/Category.cs
@@ -0,0 +1,16 @@
+using System.Text.Json.Serialization;
+using ExpensePaymentSystem.Base.Schema;
+
+namespace ExpensePaymentSystem.Schema;
+public class CategoryRequest : BaseRequest
+{
+    [JsonIgnore]
+    public int Id { get; set; }
+    public string CategoryType { get; set; }
+}
+public class CategoryResponse : BaseResponse
+{
+    public string CategoryType { get; set; }
+    public int Status { get; set; }
+
+}

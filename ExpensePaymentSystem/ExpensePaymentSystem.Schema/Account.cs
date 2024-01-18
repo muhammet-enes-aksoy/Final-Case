5f20c15b4705934da01f0d597efc701eaf367cae
muhammet-enes-aksoy <muhammed_enes43@hotmail.com>
2024-01-19 00:26:41 +0300

    Create Account.cs

---
+++ ExpensePaymentSystem/ExpensePaymentSystem.Schema/Account.cs
@@ -0,0 +1,31 @@
+using System.Text.Json.Serialization;
+using ExpensePaymentSystem.Base.Schema;
+
+
+namespace ExpensePaymentSystem.Schema;
+
+public class AccountRequest : BaseRequest
+{
+    [JsonIgnore]
+    public int AccountNumber { get; set; }
+    
+    public int UserId { get; set; }
+    public string IBAN { get; set; }
+    public decimal Balance { get; set; }
+    public string CurrencyType { get; set; }
+    public string Name { get; set; }
+}
+
+
+public class AccountResponse : BaseResponse
+{
+    public int UserId { get; set; }
+    public string UserName { get; set; }
+    public int AccountNumber { get; set; }
+    public string IBAN { get; set; }
+    public decimal Balance { get; set; }
+    public string CurrencyType { get; set; }
+    public string Name { get; set; }
+    public DateTime OpenDate { get; set; }
+
+}

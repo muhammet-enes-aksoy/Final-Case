326d9bc9da105d74a7d2ba02c9ef8e005371ff57
muhammet-enes-aksoy <muhammed_enes43@hotmail.com>
2024-01-18 23:57:17 +0300

    Created Account Entity

--- ExpensePaymentSystem/ExpensePaymentSystem.Data/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ExpensePaymentSystem.Data;
-
-public class Class1
-{
-
-}
---
+++ ExpensePaymentSystem/ExpensePaymentSystem.Data/Entities/Account.cs
@@ -0,0 +1,47 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using ExpensePaymentSystem.Base.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace ExpensePaymentSystem.Data.Entity;
+
+[Table("Account", Schema = "dbo")]
+public class Account : BaseEntity
+{
+    public int UserId { get; set; }
+    public virtual User User { get; set; }
+    public int AccountNumber { get; set; }
+    public string IBAN { get; set; }
+    public decimal Balance { get; set; }
+    public string CurrencyType { get; set; }
+    public string Name { get; set; }
+    public DateTime OpenDate { get; set; }
+
+}
+
+public class AccountConfiguration : IEntityTypeConfiguration<Account>
+{
+    public void Configure(EntityTypeBuilder<Account> builder)
+    {
+        builder.Property(x => x.AccountNumber).ValueGeneratedNever();
+        
+        builder.Property(x => x.InsertDate).IsRequired(true);
+        builder.Property(x => x.InsertUserId).IsRequired(true);
+        builder.Property(x => x.UpdateDate).IsRequired(false);
+        builder.Property(x => x.UpdateUserId).IsRequired(false);
+        builder.Property(x => x.IsActive).IsRequired(true).HasDefaultValue(true);
+
+        builder.Property(x => x.UserId).IsRequired(true);
+        builder.Property(x => x.AccountNumber).IsRequired(true);
+        builder.Property(x => x.IBAN).IsRequired(true).HasMaxLength(34);
+        builder.Property(x => x.Balance).IsRequired(true).HasPrecision(18, 4);
+        builder.Property(x => x.CurrencyType).IsRequired(true).HasMaxLength(3);
+        builder.Property(x => x.Name).IsRequired(false).HasMaxLength(100);
+        builder.Property(x => x.OpenDate).IsRequired(true);
+
+        builder.HasIndex(x => x.UserId);
+        builder.HasIndex(x => x.AccountNumber).IsUnique(true);
+        builder.HasKey(x => x.AccountNumber);
+    }
+}

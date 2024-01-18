f648b8f5791634c20b87f4f06f1f7cd356b12819
muhammet-enes-aksoy <muhammed_enes43@hotmail.com>
2024-01-18 23:58:00 +0300

    Create User Entity

---
+++ ExpensePaymentSystem/ExpensePaymentSystem.Data/Entities/User.cs
@@ -0,0 +1,38 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using ExpensePaymentSystem.Base.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace ExpensePaymentSystem.Data.Entity;
+
+[Table("User", Schema = "dbo")]
+public class User : BaseEntityWithId
+{
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Role { get; set; }
+    
+    public virtual List<Contact> Contacts { get; set; }
+    public virtual List<Account> Accounts { get; set; }
+}
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.Property(x => x.InsertDate).IsRequired(true);
+        builder.Property(x => x.InsertUserId).IsRequired(true);
+        builder.Property(x => x.UpdateDate).IsRequired(false);
+        builder.Property(x => x.UpdateUserId).IsRequired(false);
+        builder.Property(x => x.IsActive).IsRequired(true).HasDefaultValue(true);
+    
+        builder.Property(x => x.FirstName).IsRequired(true).HasMaxLength(50);
+        builder.Property(x => x.LastName).IsRequired(true).HasMaxLength(50);
+        builder.Property(x => x.Role).IsRequired(true).HasMaxLength(30);
+
+        //builder.HasIndex(x => x.UserName).IsUnique(true);
+
+    }
+}

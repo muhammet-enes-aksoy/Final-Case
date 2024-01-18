d6bbc18d7f3ed538284bfff6344d13f1f2c042d8
muhammet-enes-aksoy <muhammed_enes43@hotmail.com>
2024-01-18 23:57:28 +0300

    Create Address.cs

---
+++ ExpensePaymentSystem/ExpensePaymentSystem.Data/Entities/Address.cs
@@ -0,0 +1,44 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using ExpensePaymentSystem.Base.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace ExpensePaymentSystem.Data.Entity;
+
+[Table("Address", Schema = "dbo")]
+public class Address : BaseEntityWithId
+{
+    public int UserId { get; set; }
+    public virtual User User { get; set; }
+
+    public string Address1 { get; set; }
+    public string Address2 { get; set; }
+    public string Country { get; set; }
+    public string City { get; set; }
+    public string County { get; set; }
+    public string PostalCode { get; set; }
+    public bool IsDefault { get; set; }
+}
+public class AddressConfiguration : IEntityTypeConfiguration<Address>
+{
+    public void Configure(EntityTypeBuilder<Address> builder)
+    {
+        builder.Property(x => x.InsertDate).IsRequired(true);
+        builder.Property(x => x.InsertUserId).IsRequired(true);
+        builder.Property(x => x.UpdateDate).IsRequired(false);
+        builder.Property(x => x.UpdateUserId).IsRequired(false);
+        builder.Property(x => x.IsActive).IsRequired(true).HasDefaultValue(true);
+        
+        builder.Property(x => x.UserId).IsRequired(true);
+        builder.Property(x => x.Address1).IsRequired(true).HasMaxLength(150);
+        builder.Property(x => x.Address2).IsRequired(false).HasMaxLength(150);
+        builder.Property(x => x.Country).IsRequired(true).HasMaxLength(100);
+        builder.Property(x => x.City).IsRequired(true).HasMaxLength(100);
+        builder.Property(x => x.County).IsRequired(false).HasMaxLength(100);
+        builder.Property(x => x.PostalCode).IsRequired(false).HasMaxLength(10);
+        builder.Property(x => x.IsDefault).IsRequired(true).HasDefaultValue(false);
+        
+        builder.HasIndex(x => x.UserId);
+    }
+}

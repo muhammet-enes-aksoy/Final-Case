e90fdfe0d31dbe4030e5f8fb5e312ddda8e020c9
muhammet-enes-aksoy <muhammed_enes43@hotmail.com>
2024-01-19 00:27:21 +0300

    Create ExpenseClaim Schema

---
+++ ExpensePaymentSystem/ExpensePaymentSystem.Schema/ExpenseClaim.cs
@@ -0,0 +1,42 @@
+using System.Text.Json.Serialization;
+using ExpensePaymentSystem.Base.Schema;
+using ExpensePaymentSystem.Data.Entity;
+
+namespace ExpensePaymentSystem.Schema;
+public class ExpenseClaimRequest : BaseRequest
+{
+    [JsonIgnore]
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public virtual User User { get; set; }
+    public int CategoryId { get; set; }
+    public virtual Category Category { get; set; }
+    public int PaymentMethodId { get; set; }
+    public virtual PaymentMethod PaymentMethod { get; set; }
+    public string PaymentLocation { get; set; }
+    public string ReceiptNumber { get; set; }
+    public string Status { get; set; }
+    public string StatusDescription { get; set; }
+    public bool IsProcessed { get; set; }
+    public double Amount { get; set; }
+    public DateTime ClaimDate { get; set; }
+    public DateTime ConfirmDate { get; set; }
+}
+public class ExpenseClaimResponse : BaseResponse
+{
+    public int UserId { get; set; }
+    public virtual User User { get; set; }
+    public int CategoryId { get; set; }
+    public virtual Category Category { get; set; }
+    public int PaymentMethodId { get; set; }
+    public virtual PaymentMethod PaymentMethod { get; set; }
+    public string PaymentLocation { get; set; }
+    public string ReceiptNumber { get; set; }
+    public string Status { get; set; }
+    public string StatusDescription { get; set; }
+    public bool IsProcessed { get; set; }
+    public double Amount { get; set; }
+    public DateTime ClaimDate { get; set; }
+    public DateTime ConfirmDate { get; set; }
+
+}

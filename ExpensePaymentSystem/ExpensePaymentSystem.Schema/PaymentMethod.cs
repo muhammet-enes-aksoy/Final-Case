d7862d85e49361ebb94214aea9469996983448c8
muhammet-enes-aksoy <muhammed_enes43@hotmail.com>
2024-01-19 00:27:49 +0300

    Create PaymentMethod Schema

---
+++ ExpensePaymentSystem/ExpensePaymentSystem.Schema/PaymentMethod.cs
@@ -0,0 +1,16 @@
+using System.Text.Json.Serialization;
+using ExpensePaymentSystem.Base.Schema;
+
+namespace ExpensePaymentSystem.Schema;
+public class PaymentMethodRequest : BaseRequest
+{
+    [JsonIgnore]
+    public int Id { get; set; }
+    public string PaymentMethodType { get; set; }
+}
+public class PaymentMethodResponse : BaseResponse
+{
+    public string PaymentMethodType { get; set; }
+    public int Status { get; set; }
+
+}

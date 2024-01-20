2c6163c615a43f38046c06021dc194bde848e60a
muhammet-enes-aksoy <muhammed_enes43@hotmail.com>
2024-01-20 23:43:30 +0300

    Update program

--- ExpensePaymentSystem/ExpensePaymentSystem.Api/Program.cs
+++ ExpensePaymentSystem/ExpensePaymentSystem.Api/Program.cs
@@ -5,12 +5,7 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-        Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(config).CreateLogger();
-        Log.Information("App server is starting.");
-        
         Host.CreateDefaultBuilder(args)
-            .UseSerilog()
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
